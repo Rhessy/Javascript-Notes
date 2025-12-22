1fd3ebaeaa94d28336f30a7010635c3dba1c3a7d
Riley Hess <R.hess@beckhoff.com>
2025-12-22 12:28:12 -0600

    adding in latest javacript notes

--- Notes.js
+++ Notes.js
@@ -1,4 +1,30 @@
 // This is a place to keep track of my learning while completing the Javacript course on Udemy.
+
+// The let keyword is used to declare variables that can change later 
+// (during the execution of our program)
+let varibleName; 
+variableName = 'First assignemnt';
+variableName = 'Second assignment';  //variable was mutated to 'second assignment'
+
+// The const keyword is used to declare a variable that cannot be chagned later 
+const variableName2 = 2;
+//If you try mutate this variable it will give you an error because it is imutable
+variableName2 = 3;
+//You also cannot decalre a variable without initializing it.
+const variableName2;
+
+//The var keyword is more of a legacy way to define variables which is prior to ES6
+var job = 'programmer';
+job = 'teacher';
+
+// ** THERE IS AN IMPORTANT DIFFERENCE BETWEEN CONST, LET, AND VAR BUT IT WILL COME UP IN
+//LATER SECTIONS BLOCK VS FUNCTION SCOPE DIF. 
+
+
+//This works however, it dosen't create the variable in the scope it creates a property on
+//the global object. ALWAYS DECLARE VARIABLES..
+lastName = "last name";
+
 const age = 17;
 const isOldEnough = age >= 18;
 
@@ -9,3 +35,4 @@ if (isOldEnough) {
     const yearsLeft = 18 - age;
     console.log(`You are too young you need to wait for another ${yearsLeft} years until you can drive`);
 }
+
